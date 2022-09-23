44b3387ec2cd5fa4b3ba01f9d47121abf8750c1d
SAMUEL MARCIANO <samuel.marciano@senacsp.edu.br>
2022-09-22 22:22:33 -0300

    app mobile IMHERE incluindo nomes na lista e mostrando mensagem de erro

--- IMHERE/IMHERE/src/screens/Home/index.tsx
+++ IMHERE/IMHERE/src/screens/Home/index.tsx
@@ -1,13 +1,42 @@
-import {Text, View, TextInput, TouchableOpacity} from 'react-native'
+import {Text, View, TextInput, TouchableOpacity,ScrollView, Alert} from 'react-native'
 import {styles} from './style'
 import {Participant} from '../Components/Participantes'
+import { useState } from 'react'
 
 
 export function Home(){
 
+    // const participants = ['EU']
+
+    const [participants, setParticipants]= useState<string[]>([])
+    const [participantName,setParticipantName]= useState('')
     function handleParticipantAdd(){
-        console.log('voce Clicou no botao "+"')
+        console.log('voce Clicou no botao "+"')   
+        if(participants.includes(participantName)){
+            return Alert.alert('Participante existe ',' Já existe um participante na lista com esse nome ' )
+        }
+        // participants.push( 'Ana' )
+        // setParticipants(['Ana'])
+        // console.log(participants)
+
+        // preveState=conteúdo atal do estado
+        setParticipants(prevState => [...prevState, participantName])
+        setParticipantName('')
     }
+    function handleParticipantRemove(name:string){
+        console.log(`Você Removeu o participante ${name}`)   
+        Alert.alert('Remover',`Remover o participante ${name}?`,[
+            {
+                text:'sim',
+                onPress:() => Alert.alert('Deletado')
+            },
+            {
+                text:'Não',
+                style:'cancel'
+            },       
+        ])
+    }
+
     return(
         <View style={styles.container}>
             <Text style={styles.eventName}>
@@ -21,6 +50,8 @@ export function Home(){
                     style={styles.input}
                     placeholder='Nome do participante'
                     placeholderTextColor={"#6B6B6B"}
+                    onChangeText={setParticipantName}
+                    value={participantName}
                     // keyboardType='numeric'
                 />
                 <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
@@ -29,10 +60,20 @@ export function Home(){
                     </Text>
                 </TouchableOpacity>
             </View>
-            <Participant name="Samuel" />
-            <Participant name="kalango"/>
-            <Participant name="sei la jow"/>
-            <Participant name="jubileu"/>
+            <ScrollView showsVerticalScrollIndicator={false}>
+
+            {/* dentro das chaves estamso escrevendo JSX */}
+                {
+                participants.map(participant => (
+                    
+                    <Participant
+                    key= {participant} 
+                    name={participant} 
+                    onRemove={()=>handleParticipantRemove(participant)} />
+                    ))
+                    
+                }
+            </ScrollView>
             
 
         </View>
